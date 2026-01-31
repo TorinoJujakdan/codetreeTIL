e223744a036c20ac398197db018831ad4b6b3998
TorinoJujakdan <juinwork617@gmail.com>
2026-01-31 21:56:16 +0900

    Title: 1부터의 합, Time: 44ms, Memory: 16MB, Status: Passed - Codetree

--- 260131/1부터의 합/sum-from-the-one.py
+++ 260131/1부터의 합/sum-from-the-one.py
@@ -2,9 +2,7 @@ n = int(input())
 
 ans = 0
 for i in range(1, 101):
-    if ans < n:
-        ans += i
-        continue
-    elif ans >= n:
-        print(i - 1)
+    ans += i
+    if ans >= n:
+        print(i)
         break
